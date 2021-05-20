30cf791ac8a09d1a8cd1d2ccadc53da2aa58e276
q90016200 <q90016200@gmail.com>
2021-05-20 15:50:44 +0800

    update gameLaunch  params

--- src/Contract/TFGServiceInterfae.php
+++ src/Contract/TFGServiceInterfae.php
@@ -9,12 +9,13 @@ interface TFGServiceInterfae
      * 遊戲啟動
      *
      * @param string $op_code
+     * @param string $vendor_code
      * @param string $account
      * @param string $game_code
      * @param string $language
      * @return mixed
      */
-    function gameLaunch(string $op_code, string $account, string $game_code, string $language);
+    function gameLaunch(string $op_code, string $vendor_code, string $account, string $game_code, string $language);
 
     /**
      * 遊戲詳情
